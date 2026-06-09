95da418d9a14826481189c0e225982a7de37dc73
Emma Ivonne De la Cruz Cabrera <emma.delacruz1394@gmail.com>
2026-06-08 19:37:14 -0500

    Actualiza script de base de datos y tablas

--- OLP/create_database.sql
+++ OLP/create_database.sql
@@ -15,45 +15,24 @@ nombre_segmento VARCHAR(100) NOT NULL,
 
 ---Cliente
 
-CREATE TABLE cliente (
-id_cliente INT IDENTITY(1,1) PRIMARY KEY,
-nombre_cliente VARCHAR(100) NOT NULL,
-edad INT,
-genero VARCHAR(20),
-direccion VARCHAR(200),
-distrito VARCHAR(100),
-telefono VARCHAR(20),
-correo VARCHAR(100),
-id_segmento INT NOT NULL,
+---cliente
 
-CONSTRAINT FK_cliente_segmento
-    FOREIGN KEY (id_segmento)
-    REFERENCES segmento(id_segmento)
+CREATE TABLE cliente (
+    id_cliente INT IDENTITY(1,1) PRIMARY KEY,
+    nombre_cliente VARCHAR(100) NOT NULL,
+    edad INT,
+    genero VARCHAR(20),
+    direccion VARCHAR(200),
+    distrito VARCHAR(100),
+    telefono VARCHAR(20),
+    correo VARCHAR(100),
+    id_segmento INT NOT NULL,
+
+    CONSTRAINT FK_cliente_segmento
+        FOREIGN KEY (id_segmento)
+        REFERENCES segmento(id_segmento)
 );
 GO
-
-SELECT name
-FROM sys.tables;
-
-SELECT TABLE_SCHEMA, TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-ORDER BY TABLE_NAME;
-
-SELECT * FROM clientes;
-DROP TABLE clientes;
-GO
-
-SELECT TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES;
-
-SELECT
-    DB_NAME() AS BaseActual,
-    name AS Tabla
-FROM sys.tables;
-
-SELECT DB_NAME() AS BaseActual;
-
-SELECT * FROM cliente;
 
 ---categoria
 
@@ -63,11 +42,6 @@ CREATE TABLE categoria (
     descripcion VARCHAR(255)
 );
 GO
-
-SELECT
-    DB_NAME() AS BaseActual,
-    name AS Tabla
-FROM sys.tables;
 
 ---producto
 
@@ -116,4 +90,3 @@ CREATE TABLE detalleventa (
         REFERENCES producto(id_producto)
 );
 GO
-    
